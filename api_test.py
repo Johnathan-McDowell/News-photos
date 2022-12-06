8d78f6818929dd05d73a0dcb0d6b62c8258a66fe
Johnathan McDowell <mcdowell.johnathan@gmail.com>
2022-12-06 16:10:25 -0500

    Twitter button added, as was data selectors

--- api_test.py
+++ api_test.py
@@ -5,43 +5,50 @@ import tkinter as tk
 from PIL import Image,ImageTk
 import numpy as np
 import urllib.request   
-import random
-def printImages(date):
+import tkinter.messagebox
+def printImages(org,info,date):
     root= tk.Tk()
     # Init
     newsapi = NewsApiClient(api_key='c4560a61b5144991b4d60a735e9f5812')
 
     # /v2/top-headlines
-    top_headlines = newsapi.get_everything(sources='bbc-news',from_param=date,
-                                        to=date)
+    top_headlines = newsapi.get_everything(
+                                        from_param=date,
+                                        to=date,
+                                        domains=org, 
+                                        q = info)
     
     json_object = json.dumps(top_headlines) 
     readable_data = json.loads(json_object)
     images = []
     for i in range((len(readable_data['articles']))):
         images.append((readable_data['articles'][i]['urlToImage']))
-    images = images[7:13]
-    img = []
-    img = Image.open(urllib.request.urlopen(images[0]))
-    img1 = Image.open(urllib.request.urlopen(images[1]))
-    img2 = Image.open(urllib.request.urlopen(images[2]))
-    img3 = Image.open(urllib.request.urlopen(images[3]))
-    img4 = Image.open(urllib.request.urlopen(images[4]))
-    img5 = Image.open(urllib.request.urlopen(images[5]))
-    newsize = (300, 300)
-    canvas=tk.Canvas(root, height=600, width=900)
-    img = img.resize(newsize)
-    img1 = img1.resize(newsize)
-    img2 = img2.resize(newsize)
-    img3 = img3.resize(newsize)
-    img4 = img4.resize(newsize)
-    img5 = img5.resize(newsize)
-    AppendedImg = np.hstack((img,img1,img2))
-    gAppendedImg = np.hstack((img3,img4,img5))
-   
-    photo = ImageTk.PhotoImage(master = canvas,width = 900, height = 600,image=Image.fromarray(np.vstack((np.array(AppendedImg),np.array(gAppendedImg)))))
-    canvas.create_image(0, 0, image=photo, anchor='nw')
+    images = images[0:6]
+    try:
+        img0 = Image.open(urllib.request.urlopen(images[0]))
+        img1 = Image.open(urllib.request.urlopen(images[1]))
+        img2 = Image.open(urllib.request.urlopen(images[2]))
+        img3 = Image.open(urllib.request.urlopen(images[3]))
+        img4 = Image.open(urllib.request.urlopen(images[4]))
+        img5 = Image.open(urllib.request.urlopen(images[5]))
+        newsize = (300, 300)
+        canvas=tk.Canvas(root, height=600, width=900)
+        img0 = img0.resize(newsize)
+        img1 = img1.resize(newsize)
+        img2 = img2.resize(newsize)
+        img3 = img3.resize(newsize)
+        img4 = img4.resize(newsize)
+        img5 = img5.resize(newsize)
+        AppendedImg = np.hstack((img0,img1,img2))
+        gAppendedImg = np.hstack((img3,img4,img5))
+    
+        photo = ImageTk.PhotoImage(master = canvas,width = 900, height = 600,image=Image.fromarray(np.vstack((np.array(AppendedImg),np.array(gAppendedImg)))))
+        canvas.create_image(0, 0, image=photo, anchor='nw')
+        canvas.pack(side = tk.TOP, expand=True, fill=tk.BOTH)
+    except:
+        tkinter.messagebox.showinfo("ERROR.",  "Not enough pictures exist for this promp, please enter new values")
+    
 
-    canvas.pack(side = tk.TOP, expand=True, fill=tk.BOTH)
+    
     root.mainloop()
         
--- day_in_photos_gui.py
+++ day_in_photos_gui.py
@@ -5,7 +5,9 @@ import pandas as pd
 import customtkinter
 import quotesAPI as quote
 import api_test as api
-
+from PIL import Image, ImageTk
+import os
+PATH = os.path.dirname(os.path.realpath(__file__))
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -15,14 +17,16 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         WIDTH = 480
-        HEIGHT = 580
+        HEIGHT = 600
+        self.info= ""
+        self.org= ""
         self.text = quote.quotes()
         self.geometry(f"{WIDTH}x{HEIGHT}")
         self.title("Your Week in The News")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1, minsize=250)
-        
+        self.logo = self.load_image("\logo.png", 20)
         self.frame_1 = customtkinter.CTkFrame(master=self, width=300, height=240, corner_radius=15)
         self.frame_1.grid(row=0, column=0, padx=20, pady=20, columnspan=2 ,sticky="nsew")
         self.frame_1.grid_columnconfigure(0, weight=1)
@@ -36,55 +40,58 @@ class App(customtkinter.CTk):
                                               text_font=("Roboto Medium", -20))  
         
         self.label_1.grid(row=1, column=0, pady=20,columnspan=2 ,sticky="new")
-        self.label_mode = customtkinter.CTkLabel(master=self.frame_1, text="News Category:")
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_1, text="News Organization:")
         self.label_mode.grid(row=2, column=3, pady=0, padx=20, sticky="w")
+        optionmenu_var = customtkinter.StringVar(value="News Orgs")
         self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_1,
-                                                        values=["Politics", "Culture", "Culinary"])
+                                                        values=["cnn.com", "techcrunch.com", "theguardian.com","theverge.com"],
+                                                        command=self.optionmenu_callback,
+                                                        variable=optionmenu_var)
         self.optionmenu_1.grid(row=3, column=3, padx=20, sticky="w")
         
-        self.label_mode = customtkinter.CTkLabel(master=self.frame_1, text="News Organization:")
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_1, text="Filter By Keyword:")
         self.label_mode.grid(row=4, column=3, pady=0, padx=20, sticky="w")
-        self.optionmenu_2 = customtkinter.CTkOptionMenu(master=self.frame_1,
-                                                        values=["BBC", "New York Times", "Washington Post"],
-                                                        )
-        self.optionmenu_2.grid(row=5, column=3, pady=10, padx=20, sticky="w")
+        strVar = customtkinter.StringVar()
+        self.entry = customtkinter.CTkEntry(master=self.frame_1,textvariable=strVar)
+        self.entry.bind("<Return>",func=lambda event, strVar=strVar: self.obtain(strVar))
+        self.entry.grid(row=5, column=3, pady=10, padx=20, sticky="w")
 
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_1, text="Tweet Your Images!")
+        self.label_mode.grid(row=6, column=3, pady=0, padx=20, sticky="w")
+        self.tweet = customtkinter.CTkButton(master=self.frame_1, image=self.logo, text="Twitter", height=32, compound="right")
+        self.tweet.grid(row=7, column=3, padx=20, pady=10, sticky="w")
+        
         dates = []
         day_of_week= []
-        
-        date1 = date.today()
         for i in range(7):
             dates.append(date.today() - timedelta(days = i))
             day_of_week.append(pd.Timestamp(dates[i]))
            
-        buttons0=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[0].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[0]))
+        buttons0=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[0].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[0]))
         buttons0.grid(row=2, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons1=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[1].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[1]))
+        buttons1=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[1].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[1]))
         buttons1.grid(row=3, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons2=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[2].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[2]))
+        buttons2=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[2].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[2]))
         buttons2.grid(row=4, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons3=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[3].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[3]))
+        buttons3=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[3].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[3]))
         buttons3.grid(row=5, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons4=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[4].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[4]))
+        buttons4=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[4].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[4]))
         buttons4.grid(row=6, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons5=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[5].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[5]))
+        buttons5=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[5].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[5]))
         buttons5.grid(row=7, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
-        buttons6=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[6].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(dates[6]))
+        buttons6=customtkinter.CTkButton(master=self.frame_1, text= day_of_week[6].day_name(), height=32,compound="right",hover_color="#C77C78",command =lambda:api.printImages(self.org,self.info,dates[6]))
         buttons6.grid(row=8, column=0, columnspan=2, padx=20, pady=(10), sticky="ew")
         self.quote_text = customtkinter.CTkLabel(master=self.frame_2, text=self.text,wraplength=300, justify="center", text_font=("Roboto Medium", -14))
         self.quote_text.grid(row=0, column=0,padx =20,pady =10,sticky="news")
         
     def on_closing(self, event=0):
         self.destroy()
-        ''''
-    def change_station(self, new_station):
-        if(new_station.equals('BBC'))
-        {
-           new_station =  
-        }
-        self.station = new_station
-'''
-
+    def obtain(self, strVar):
+        self.info= strVar.get()
+    def load_image(self, path, image_size):
+        return ImageTk.PhotoImage(Image.open(PATH + path).resize((image_size, image_size)))
+    def optionmenu_callback(self,choice):
+        self.org = choice
 if __name__ == "__main__":
     app = App()
     app.mainloop()
